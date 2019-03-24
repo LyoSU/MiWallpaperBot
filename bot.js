0b9a741cbe2e98d2f416dc19ca8c06de9f53fd0a
LyoSU <ua.lyo.su@gmail.com>
2019-03-24 23:43:27 +0200

    1.0.1

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ bot.telegram.getMe().then((botInfo) => {
   bot.options.username = botInfo.username
 })
 
-bot.start((ctx) => ctx.replyWithHTML('Привет я выдаю обои из Mi Wallpaper Carousel\n\n<b>Список категорий:</b> /category\n<b>Случайные обои:</b> /mi\n\nРазработчик: @LyoSU'))
+bot.start((ctx) => ctx.replyWithHTML('Привет! Я выдаю обои из Mi Wallpaper Carousel\n\n<b>Список категорий:</b> /category\n<b>Случайные обои:</b> /mi\n\nРазработчик: @LyoSU\n\n<a href="https://github.com/LyoSU/MiWallpaperBot">GitHub</a>'))
 
 bot.hears(/(?:\/mi)((?:_|)(\w+)|)/, async (ctx) => {
   ctx.replyWithChatAction('upload_photo')
